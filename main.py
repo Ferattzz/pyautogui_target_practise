ce8d8465dd15d7a1439d9cf0bf1a4d56f47e94bc
federicocotogno <federicosou@fastmail.se>
2021-06-04 10:06:36 +0200

    project

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Clicker:
 
     def nav_to_image(self):
         try:
-            position = pt.locateOnScreen(self.target_png, confidence=.6, region=(0, 84, 1277, 793))
+            position = pt.locateOnScreen(self.target_png, confidence=.6)  # region=(0, 84, 1277, 793)
             pt.moveTo(position[0] + 15, position[1] + 15, duration=self.speed)
             pt.doubleClick()
 
